33af8c3eff4c9822e447fca5b58e98f60c5ab0e9
echo <echo@arbitraryexecution.com>
2021-09-29 08:05:26 -0400

    AE-483: Moved contract names and events to JSON file.  Linting fixes.

--- src/ownership-change/ownership-change.js
+++ src/ownership-change/ownership-change.js
@@ -7,11 +7,8 @@ const { perpfiEverestId } = require('../../agent-config.json');
 // import helper functions for loading the perp.fi contract addresses and abis
 const common = require('../common');
 
-// specify the contracts that we would like to monitor
-const contractNames = ['ClearingHouse', 'InsuranceFund'];
-
-// specify the event(s) that we want to monitor for
-const events = ['OwnershipTransferred'];
+// load the contracts and events that we would like to monitor
+const contractNamesEvents = require('./ownership-change-events.json');
 
 // this is a toggle for using V1 instead of V2 for contracts that specify both
 // i.e. ClearingHouse.sol and ClearingHouseV1.sol are both contracts
@@ -19,7 +16,7 @@ const useLegacy = true;
 
 // iterate over the contracts to get their addresses and abis
 const contractAddressesAbisEvents = [];
-contractNames.forEach((contractName) => {
+(Object.keys(contractNamesEvents)).forEach((contractName) => {
   // get the contract address
   const address = common.getAddressLayer2(contractName);
 
@@ -32,9 +29,9 @@ contractNames.forEach((contractName) => {
   contractAddressesAbisEvents.push({
     name: contractName,
     address: address.toLowerCase(),
-    abi: abi,
-    events: events,
-    iface: iface,
+    abi,
+    events: contractNamesEvents[contractName],
+    iface,
   });
 });
 
@@ -50,32 +47,31 @@ function createAlert(log, eventName, contractName, contractAddress) {
   // if this is an ownership transfer event, add the new owner address to the finding
   if (log.name === 'OwnershipTransferred') {
     // signature is OwnershipTransferred(address indexed previousOwner, address indexed newOwner)
-    //   args[0] is previousOwner
-    //   args[1] is newOwner
-    metadata['newOwner'] = log.args[1];
+    //   args[0] is previousOwner, accessible using args.previousOwner
+    //   args[1] is newOwner, accesible using args.newOwner
+    metadata.newOwner = log.args.newOwner;
   }
 
   return Finding.fromObject({
-    name: `Perp.Fi Admin Event`,
+    name: 'Perp.Fi Admin Event',
     description: `The ${eventName} event was emitted by the ${contractName} contract`,
     alertId: 'AE-PERPFI-ADMIN-EVENT',
     protocol: 'Perp.Fi',
     severity: FindingSeverity.Low,
     type: FindingType.Suspicious,
     everestId: perpfiEverestId,
-    metadata: metadata,
+    metadata,
   });
 }
 
 // helper function to filter logs based on contract addresses and event names
 function filterAndParseLogs(logs, contractData) {
-
   // collect logs only from the contracts of interest
-  const perpfiLogs = logs.filter((log) => log.address === contractData['address']);
+  const perpfiLogs = logs.filter((log) => log.address === contractData.address);
 
   // decode logs and filter on the ones we are interested in
-  const parse = (log) => contractData['iface'].parseLog(log);
-  const filter = (log) => (contractData['events']).indexOf(log.name) !== -1;
+  const parse = (log) => contractData.iface.parseLog(log);
+  const filter = (log) => (contractData.events).indexOf(log.name) !== -1;
   const parsedLogs = perpfiLogs.map(parse).filter(filter);
 
   return parsedLogs;
@@ -84,14 +80,13 @@ function filterAndParseLogs(logs, contractData) {
 async function handleTransaction(txEvent) {
   const findings = [];
 
-  const logs = txEvent.logs;
+  const { logs } = txEvent;
 
   contractAddressesAbisEvents.forEach((contractData) => {
     const parsedLogs = filterAndParseLogs(logs, contractData);
     parsedLogs.forEach((log) => {
-      findings.push(createAlert(log, log.name, contractData['name'], contractData['address']));
+      findings.push(createAlert(log, log.name, contractData.name, contractData.address));
     });
-
   });
   return findings;
 }

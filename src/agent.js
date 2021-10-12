4cc1d59c6ac4d84b4f0e9db4f8407870efcd67e8
yowl <yowl@arbitraryexecution.com>
2021-10-12 14:52:46 -0400

    moved forta v0.0.18+ compatible agent.js into repo

--- src/agent.js
+++ src/agent.js
@@ -1,16 +1,61 @@
-// import handlers
-//const exampleHandler = require('./path/to/example-handler');
+// transaction handlers
+// <none>
 
-const handleTransaction = async (txEvent) => {
-  const findings = (
-    await Promise.all([
-      // add handleTransaction from handlers
-      // exampleHandler.handleTransaction(txEvent),
-    ])
-  ).flat();
-  return findings;
-};
+// block handlers
+// <none>
+
+const txHandlers = [
+  // list txHandlers like this `optimisticOracle,` +++++ REMOVE ME +++++
+  // <none>
+];
+
+const blockHandlers = [
+  // <none>
+];
+
+// returns findings over all txHandler's handleTransaction functions
+function provideHandleTransaction(agents) {
+  return async function handleTransaction(txEvent) {
+    const findings = (
+      await Promise.all(
+        agents.map((agent) => agent.handleTransaction(txEvent)),
+      )
+    ).flat();
+
+    return findings;
+  };
+}
+
+// returns findings over all blockHandler's handleBlock functions
+function provideHandleBlock(agents) {
+  return async function handleBlock(blockEvent) {
+    const findings = (
+      await Promise.all(
+        agents.map((agent) => agent.handleBlock(blockEvent)),
+      )
+    ).flat();
+
+    return findings;
+  };
+}
+
+// returns a promise of all the async initialize calls
+function provideInitialize(agents) {
+  return async function initialize() {
+    return Promise.all(agents.map(async (agent) => {
+      if (typeof agent.initialize === 'function') {
+        return agent.initialize();
+      }
+      return Promise.resolve();
+    }));
+  };
+}
 
 module.exports = {
-  handleTransaction,
+  provideInitialize,
+  initialize: provideInitialize([...txHandlers, ...blockHandlers]),
+  provideHandleTransaction,
+  handleTransaction: provideHandleTransaction(txHandlers),
+  provideHandleBlock,
+  handleBlock: provideHandleBlock(blockHandlers),
 };

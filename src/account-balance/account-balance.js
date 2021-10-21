577ca2f3d9dbf546895bb7374e858c52f39a8092
arbitrary-sparrow <92380866+arbitrary-sparrow@users.noreply.github.com>
2021-10-21 09:49:35 -0400

    Reduce account balance agent reporting frequency to every N hours

--- src/account-balance/account-balance.js
+++ src/account-balance/account-balance.js
@@ -1,3 +1,4 @@
+const BigNumber = require('bignumber.js');
 const ethers = require('ethers');
 const {
   getJsonRpcUrl, Finding, FindingSeverity, FindingType,
@@ -14,18 +15,26 @@ function provideInitialize(data) {
   return async function initialize() {
     /* eslint-disable no-param-reassign */
     // assign configurable fields
-    data.accountThresholds = config.accountBalance;
+    data.accountThresholds = config.accountBalance.accountThresholds;
+    data.alertMinimumIntervalSeconds = config.accountBalance.alertMinimumIntervalSeconds;
     data.everestId = config.PERPFI_EVEREST_ID;
 
     data.accountAddresses = accountAddressesData;
     data.accountNames = Object.keys(data.accountThresholds);
     data.provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
+
+    // initialize the start time and number of alerts since last finding for each account to 0
+    data.startTime = 0;
+    data.numAlertsSinceLastFinding = {};
+    Object.values(data.accountAddresses).forEach((address) => {
+      data.numAlertsSinceLastFinding[address] = 0;
+    });
     /* eslint-enable no-param-reassign */
   };
 }
 
 // helper function to create alerts
-function createAlert(accountName, accountBalance, thresholdEth, everestId) {
+function createAlert(accountName, accountBalance, thresholdEth, everestId, numAlerts) {
   const threshold = ethers.utils.parseEther(thresholdEth.toString());
   return Finding.fromObject({
     name: 'Perp.Fi Low Account Balance',
@@ -39,12 +48,15 @@ function createAlert(accountName, accountBalance, thresholdEth, everestId) {
       accountName,
       accountBalance: accountBalance.toString(),
       threshold: threshold.toString(),
+      numAlertsSinceLastFinding: numAlerts.toString(),
     },
   });
 }
 
 function provideHandleBlock(data) {
-  return async function handleBlock() {
+  return async function handleBlock(blockEvent) {
+    // upon the mining of a new block, check the specified accounts to make sure the balance of
+    // each account has not fallen below the specified threshold
     const findings = [];
     const {
       accountThresholds, accountAddresses, accountNames, provider, everestId,
@@ -53,19 +65,41 @@ function provideHandleBlock(data) {
       throw new Error('handleBlock called before initialization');
     }
 
+    // get the block timestamp
+    const blockTimestamp = new BigNumber(blockEvent.block.timestamp);
+    let minTimeElapsed = false;
+
     await Promise.all(accountNames.map(async (accountName) => {
       const accountBalance = await provider.getBalance(accountAddresses[accountName]);
 
+      /* eslint-disable no-param-reassign */
       // If balance < threshold add an alert to the findings
       if (accountBalance < (accountThresholds[accountName] * 1000000000000000000)) {
-        findings.push(createAlert(
-          accountName,
-          accountBalance,
-          accountThresholds[accountName],
-          everestId,
-        ));
+        // if less than the specified number of hours has elapsed, just increment the counter for
+        // the number of alerts that would have been generated
+        if (blockTimestamp.minus(data.startTime) < data.alertMinimumIntervalSeconds) {
+          data.numAlertsSinceLastFinding[accountName] += 1;
+        } else {
+          minTimeElapsed = true;
+
+          findings.push(createAlert(
+            accountName,
+            accountBalance,
+            accountThresholds[accountName],
+            everestId,
+            data.numAlertsSinceLastFinding,
+          ));
+
+          // restart the alert counter
+          data.numAlertsSinceLastFinding[accountName] = 0;
+        }
       }
     }));
+
+    if (minTimeElapsed) {
+      data.startTime = new BigNumber(blockTimestamp.toString());
+    }
+    /* eslint-enable no-param-reassign */
 
     return findings;
   };

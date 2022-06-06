084bb5f21fc3ab2bebeabc17f8ca82e27f80ccd3
Tayfun Sur <tayfunsur@gmail.com>
2022-06-06 19:15:16 +0300

    Update test case with optimism address

--- admin-events/src/agent.spec.js
+++ admin-events/src/agent.spec.js
@@ -1,13 +1,16 @@
-const ethers = require('ethers');
+const ethers = require("ethers");
 const {
-  Finding, FindingType, FindingSeverity, createTransactionEvent,
-} = require('forta-agent');
+  Finding,
+  FindingType,
+  FindingSeverity,
+  createTransactionEvent,
+} = require("forta-agent");
 
-const { provideHandleTransaction, provideInitialize } = require('./agent');
+const { provideHandleTransaction, provideInitialize } = require("./agent");
 
 // tests
-describe('admin event monitoring', () => {
-  describe('handleTransaction', () => {
+describe("admin event monitoring", () => {
+  describe("handleTransaction", () => {
     let initializeData;
     let handleTransaction;
 
@@ -15,18 +18,16 @@ describe('admin event monitoring', () => {
       initializeData = {};
 
       // initialize the handler
-      await (provideInitialize(initializeData))();
+      await provideInitialize(initializeData)();
       handleTransaction = provideHandleTransaction(initializeData);
     });
 
-    it('returns empty findings if contract address does not match', async () => {
+    it("returns empty findings if contract address does not match", async () => {
       // logs data for test case:  no address match + no topic match
       const logsNoMatchAddress = [
         {
           address: ethers.constants.AddressZero,
-          topics: [
-            ethers.constants.HashZero,
-          ],
+          topics: [ethers.constants.HashZero],
         },
       ];
 
@@ -43,12 +44,12 @@ describe('admin event monitoring', () => {
       expect(findings).toStrictEqual([]);
     });
 
-    it('returns empty findings if contract address matches but not event', async () => {
+    it("returns empty findings if contract address matches but not event", async () => {
       const { contracts } = initializeData;
 
       // retrieve the Object corresponding to the InsuranceFund contract
       let index = 0;
-      while (index < contracts.length && contracts[index].name !== 'InsuranceFund') {
+      while (index < contracts.length && contracts[index].name !== "InsuranceFund") {
         index++;
       }
       const insuranceFundContract = contracts[index];
@@ -58,11 +59,11 @@ describe('admin event monitoring', () => {
         {
           address: insuranceFundContract.address,
           topics: [
-            ethers.utils.keccak256(ethers.utils.toUtf8Bytes('Paused(address)')),
+            ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Paused(address)")),
             ethers.constants.HashZero, // account
           ],
           // create a large dummy array to give ethers.parseLog() something to decode
-          data: `0x${'0'.repeat(1000)}`,
+          data: `0x${"0".repeat(1000)}`,
         },
       ];
 
@@ -79,12 +80,12 @@ describe('admin event monitoring', () => {
       expect(findings).toStrictEqual([]);
     });
 
-    it('returns a finding if a target contract emits an event from its watchlist', async () => {
+    it("returns a finding if a target contract emits an event from its watchlist", async () => {
       const { contracts } = initializeData;
 
       // retrieve the Object corresponding to the InsuranceFund contract
       let index = 0;
-      while (index < contracts.length && contracts[index].name !== 'InsuranceFund') {
+      while (index < contracts.length && contracts[index].name !== "InsuranceFund") {
         index++;
       }
       const insuranceFundContract = contracts[index];
@@ -94,12 +95,14 @@ describe('admin event monitoring', () => {
         {
           address: insuranceFundContract.address,
           topics: [
-            ethers.utils.keccak256(ethers.utils.toUtf8Bytes('OwnershipTransferred(address,address)')),
+            ethers.utils.keccak256(
+              ethers.utils.toUtf8Bytes("OwnershipTransferred(address,address)")
+            ),
             ethers.constants.HashZero, // previousOwner
             ethers.constants.HashZero, // newOwner
           ],
           // create a large dummy array to give ethers.parselog() something to decode
-          data: `0x${'0'.repeat(1000)}`,
+          data: `0x${"0".repeat(1000)}`,
         },
       ];
 
@@ -113,23 +116,26 @@ describe('admin event monitoring', () => {
       const findings = await handleTransaction(txEvent);
 
       // create expected finding
-      const testFindings = [Finding.fromObject({
-        name: 'Perpetual Finance Admin Event',
-        description: 'The OwnershipTransferred event was emitted by the InsuranceFund contract',
-        alertId: 'AE-PERPFI-ADMIN-EVENT',
-        severity: FindingSeverity.High,
-        type: FindingType.Suspicious,
-        protocol: 'Perp.Fi',
-        metadata: {
-          contractName: 'InsuranceFund',
-          contractAddress: '0x493b4455c6bee3cc9525234605704c7450171591',
-          eventName: 'OwnershipTransferred',
-          eventArgs: {
-            previousOwner: '0x0000000000000000000000000000000000000000',
-            newOwner: '0x0000000000000000000000000000000000000000',
+      const testFindings = [
+        Finding.fromObject({
+          name: "Perpetual Finance Admin Event",
+          description:
+            "The OwnershipTransferred event was emitted by the InsuranceFund contract",
+          alertId: "AE-PERPFI-ADMIN-EVENT",
+          severity: FindingSeverity.High,
+          type: FindingType.Suspicious,
+          protocol: "Perp.Fi",
+          metadata: {
+            contractName: "InsuranceFund",
+            contractAddress: "0x1c9a192df3936cbf093d8afdc352718bcf834eb6",
+            eventName: "OwnershipTransferred",
+            eventArgs: {
+              previousOwner: "0x0000000000000000000000000000000000000000",
+              newOwner: "0x0000000000000000000000000000000000000000",
+            },
           },
-        },
-      })];
+        }),
+      ];
 
       expect(findings).toStrictEqual(testFindings);
     });
